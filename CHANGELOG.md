5815b482dcca51ca4d9dda80b26f15125df323dc
Armando Lüscher <armando@noplanman.ch>
2019-04-15 01:14:40 +0200

    Announcements for releases 🌟 closes #17

---
+++ CHANGELOG.md
@@ -0,0 +1,19 @@
+# Changelog
+The format is based on [Keep a Changelog](http://keepachangelog.com/) and this project adheres to [Semantic Versioning](http://semver.org/).
+
+Exclamation symbols (:exclamation:) note something of importance e.g. breaking changes. Click them to learn more.
+
+## [Unreleased]
+### Added
+- First minor version that contains the basic functionality.
+- Simple logging of incoming webhook requests from GitHub and Travis-CI.
+- Post welcome messages to PHP Telegram Bot Support group.
+- Post release announcements to PHP Telegram Bot Support group. (#17)
+- Extended `.env.example` file.
+### Changed
+### Deprecated
+### Removed
+### Fixed
+### Security
+
+[Unreleased]: https://github.com/php-telegram-bot/support-bot/compare/master...develop
--- public/webhooks/github.php
+++ public/webhooks/github.php
@@ -9,6 +9,13 @@
  * file that was distributed with this source code.
  */
 
+use Github\Api\Issue;
+use Github\Api\PullRequest;
+use Github\Client;
+use Longman\TelegramBot\Entities\ServerResponse;
+use Longman\TelegramBot\Exception\TelegramException;
+use Longman\TelegramBot\Telegram;
+use Longman\TelegramBot\TelegramLog;
 use NPM\ServiceWebhookHandler\Handlers\GitHubHandler;
 use TelegramBot\SupportBot\Webhooks\Utils;
 
@@ -24,3 +31,126 @@ if (!$webhook->validate()) {
 
 // Save all incoming data to a log file for future reference.
 Utils::logWebhookData(getenv('TG_LOGS_DIR') . '/' . getenv('TG_BOT_USERNAME') . '_webhook_github.log');
+
+// Limit repos and events to serve.
+$allowed_repos_events = [
+    'php-telegram-bot/core'                 => ['release'],
+    'php-telegram-bot/support-bot'          => ['release'],
+    'php-telegram-bot/telegram-bot-manager' => ['release'],
+];
+
+// Get the incoming webhook data.
+$data = $webhook->getData();
+
+// Only react to allowed repos and events.
+$repo = $data['repository'];
+if (!in_array($webhook->getEvent(), $allowed_repos_events[$repo['full_name']] ?? [], true)) {
+    die;
+}
+
+// Handle event.
+switch ($webhook->getEvent()) {
+    case 'release':
+        handleRelease($data);
+        break;
+}
+
+/**
+ * Handle the "release" event.
+ *
+ * @param array $data
+ */
+function handleRelease($data) {
+    $repo    = $data['repository'];
+    $release = $data['release'];
+
+    if (!$release['draft'] && !$release['prerelease']) {
+        $author     = $release['author']['login'];
+        $author_url = $release['author']['html_url'];
+        $tag        = $release['tag_name'];
+        $url        = $release['html_url'];
+        $body       = parseReleaseBody($release['body'], $repo['owner']['login'], $repo['name']);
+
+        $message = LitEmoji\LitEmoji::encodeUnicode("
+:star: *New Release!* :star:
+(_version_ [{$tag}]({$url}) _of_ [{$repo['full_name']}]({$repo['html_url']}) _has just been released by_ [{$author}]({$author_url}))
+
+{$body}
+");
+
+        // Post the release message!
+        sendTelegramMessage(getenv('TG_SUPPORT_GROUP_ID'), $message);
+    }
+}
+
+/**
+ * Make the release message Telegram-friendly and resolve links to GitHub.
+ *
+ * @param string $body
+ * @param string $user
+ * @param string $repo
+ *
+ * @return string
+ */
+function parseReleaseBody($body, $user, $repo) {
+    // Replace headers with bold text.
+    $body = preg_replace_callback('~### (?<header>.*)~', static function ($matches) {
+        return "*{$matches['header']}*";
+    }, $body);
+
+    $github_client = new Client();
+    // Replace any ID links with the corresponding issue or pull request link.
+    $body = preg_replace_callback('~(?:(?<user>[0-9a-z\-]*)/(?<repo>[0-9a-z\-]*))?#(?<id>\d*)~i', static function ($matches) use ($github_client, $user, $repo) {
+        $text = $matches[0];
+        $id   = $matches['id'];
+        $user = $matches['user'] ?: $user;
+        $repo = $matches['repo'] ?: $repo;
+
+        // Check if this ID is an issue.
+        try {
+            /** @var Issue $issue */
+            $issue = $github_client->api('issue')->show($user, $repo, $id);
+            return "[{$text}]({$issue['html_url']})";
+        } catch (Throwable $e) {
+            // Silently ignore.
+        }
+
+        // Check if this ID is a pull request.
+        try {
+            /** @var PullRequest $pr */
+            $pr = $github_client->api('pull_request')->show($user, $repo, $id);
+            return "[{$text}]({$pr['html_url']})";
+        } catch (Throwable $e) {
+            // Silently ignore.
+        }
+
+        return $text;
+    }, $body);
+
+    return $body;
+}
+
+/**
+ * Send a text to the passed chat.
+ *
+ * @param string $chat_id
+ * @param string $text
+ *
+ * @return ServerResponse|null
+ */
+function sendTelegramMessage($chat_id, $text) {
+    try {
+        new Telegram(getenv('TG_API_KEY'));
+        TelegramLog::initErrorLog(getenv('TG_LOGS_DIR') . '/error.releases.log');
+
+        $parse_mode = 'markdown';
+
+        return Longman\TelegramBot\Request::sendMessage(compact('chat_id', 'text', 'parse_mode'));
+    } catch (TelegramException $e) {
+        TelegramLog::error($e->getMessage());
+    } catch (Throwable $e) {
+        // Silently ignore.
+    }
+
+    return null;
+}
